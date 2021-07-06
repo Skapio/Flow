969bfa56aa4eef88af082cf78c2bfeaf7a79a4ec
Pasiut_Artur <pasiut1998@gmail.com>
2021-07-06 13:18:03 +0200

    Utilize Flow Operators

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,20 +6,50 @@ import kotlinx.coroutines.withTimeoutOrNull
 
 fun main() = runBlocking {
 
-  fun starWarsSounds() = flow {
-    for (sound in listOf(chewieSound, r2d2Sound, blasterSound, saberSound)) {
-      delay(DELAY)
-      log("Emitting $sound")
-      emit(sound)
+  exampleOf("filter operator")
+
+  forceUsers.asFlow()
+    .filter { forceUser -> forceUser is Jedi }
+    .collect { forceUser -> log(forceUser.name) }
+
+  exampleOf("map operator")
+
+  suspend fun bestowSithTitle(forceUser: ForceUser): String {
+    delay(DELAY)
+    return "Darth ${forceUser.name}"
+  }
+
+  val sith = forceUsers.asFlow()
+    .filter { forceUser -> forceUser is Sith }
+    .map { sith -> bestowSithTitle(sith)}
+
+  sith.collect { sithName -> log(sithName) }
+
+
+  exampleOf("transform operator")
+
+  forceUsers.asFlow()
+    .transform { forceUser ->
+       if (forceUser is Sith) {
+         emit("Turning ${forceUser.name} to the Dark Side")
+         emit(bestowSithTitle(forceUser))
+       }
     }
-  }
+    .collect { log(it) }
 
-  withTimeoutOrNull((3.1 * DELAY).toLong()) {
-    starWarsSounds().collect { sound -> log(sound) }
-  }
+  exampleOf("size-limiting operators")
 
-  log("Done emitting sounds")
+  sith.take(2).collect { log(it) }
 
+
+  exampleOf("terminal operators")
+
+  val jefiLineage = forceUsers.asFlow()
+    .filter { it is Jedi }
+    .map { it.name }
+    .reduce { a, b -> "$a trained by $b"}
+
+  log(jefiLineage)
 }
 
 
